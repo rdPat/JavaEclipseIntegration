1331d32a10fce45760ad183f78598d8c7ee77ad9
rampa <rampa@Ram>
2024-08-03 02:44:35 +0530

    second commit

--- Event Management System/src/src/Test.java
+++ Event Management System/src/src/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i=0;i<5;i++) {
-			System.out.println("Hello ddnnworld");
+			System.out.println("Testing");
 			}
 	}
 
